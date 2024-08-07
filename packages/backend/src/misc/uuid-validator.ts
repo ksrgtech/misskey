2f852edd6910c02afe17f8ee11cfd83b78da5ba9
Kisaragi Marine <kisaragi.effective@gmail.com>
2024-08-07 16:18:02 +0900

    fix: assert both version and variant

--- packages/backend/src/misc/uuid-validator.ts
+++ packages/backend/src/misc/uuid-validator.ts
@@ -43,17 +43,21 @@ export declare namespace UUIDv4 {
 	type AssertHex<S extends string> = Hex.Assert<S>;
 	type LengthS<S extends string> = FoldableString.LengthS<S>;
 
+	type AcceptedVariant = '8' | '9' | 'a' | 'b' | 'A' | 'B';
+
 	export type Assert<S extends string> = string extends S
 		// not a literal, skipping every assertion
 		? S
 		: S extends `${infer SA}-${infer SB}-${infer SC}-${infer SD}-${infer SE}`
 			? SC extends `4${string}`
-				? [LengthS<SA>, LengthS<SB>, LengthS<SC>, LengthS<SD>, LengthS<SE>] extends [8, 4, 4, 4, 12]
-					? [AssertHex<SA>, AssertHex<SB>, AssertHex<SC>, AssertHex<SD>, AssertHex<SE>] extends [string, string, string, string, string]
-						? S
-						: AssertionError<[AssertHex<SA>, AssertHex<SB>, AssertHex<SC>, AssertHex<SD>, AssertHex<SE>]>
-					: AssertionError<`component lengths are required to be [8, 4, 4, 4, 12], but are [${LengthS<SA>}, ${LengthS<SB>}, ${LengthS<SC>}, ${LengthS<SD>}, ${LengthS<SE>}]`>
-				: AssertionError<`${SC} contains invalid variant`>
+				? SD extends `${AcceptedVariant}${string}`
+					? [LengthS<SA>, LengthS<SB>, LengthS<SC>, LengthS<SD>, LengthS<SE>] extends [8, 4, 4, 4, 12]
+						? [AssertHex<SA>, AssertHex<SB>, AssertHex<SC>, AssertHex<SD>, AssertHex<SE>] extends [string, string, string, string, string]
+							? S
+							: AssertionError<[AssertHex<SA>, AssertHex<SB>, AssertHex<SC>, AssertHex<SD>, AssertHex<SE>]>
+						: AssertionError<`component lengths are required to be [8, 4, 4, 4, 12], but are [${LengthS<SA>}, ${LengthS<SB>}, ${LengthS<SC>}, ${LengthS<SD>}, ${LengthS<SE>}]`>
+					: AssertionError<`${SD} contains invalid, or reserved variant`>
+				: AssertionError<`${SC} contains invalid version`>
 			: AssertionError<`${S} is not a hyphnated-UUIDv4`>
 
 	type AssertionError<Message> = {[notUUID]: Message};
--- packages/backend/test/unit/misc/identifiable-error.ts
+++ packages/backend/test/unit/misc/identifiable-error.ts
@@ -16,34 +16,82 @@ describe('identifiable-error', () => {
 			const excessComponents: ExpectAssertionError<UUIDv4.Assert<"a-b-c-d-e-f">> = errorIsExpected;
 			const tooShortComponents: ExpectAssertionError<UUIDv4.Assert<"a-b-c-d-e">> = errorIsExpected;
 			const tooLongComponents: ExpectAssertionError<UUIDv4.Assert<"aaaa-bbbbbbbb-4ccccccc-dddddddd-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee">> = errorIsExpected;
-			const invalidVariant0: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-0123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariant1: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-1123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariant2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-2123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariant3: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-3123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariant5: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-5123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariant6: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-6123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariant7: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-7123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariant8: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-8123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariant9: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-9123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantA: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-a123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantB: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-b123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantC: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-c123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantD: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-d123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantE: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-e123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantF: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-f123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantA2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-A123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantB2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-B123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantC2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-C123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantD2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-D123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantE2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-E123-5678-1234567890ab">> = errorIsExpected;
-			const invalidVariantF2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-F123-5678-1234567890ab">> = errorIsExpected;
+
+			const invalidVersion0: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-0123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersion1: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-1123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersion2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-2123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersion3: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-3123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersion5: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-5123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersion6: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-6123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersion7: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-7123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersion8: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-8123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersion9: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-9123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionA: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-a123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionB: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-b123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionC: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-c123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionD: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-d123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionE: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-e123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionF: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-f123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionA2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-A123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionB2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-B123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionC2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-C123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionD2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-D123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionE2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-E123-90ab-1234567890ab">> = errorIsExpected;
+			const invalidVersionF2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-F123-90ab-1234567890ab">> = errorIsExpected;
+
+			const invalidVariant0: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-00ab-1234567890ab">> = errorIsExpected;
+			const invalidVariant1: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-10ab-1234567890ab">> = errorIsExpected;
+			const invalidVariant2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-20ab-1234567890ab">> = errorIsExpected;
+			const invalidVariant3: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-30ab-1234567890ab">> = errorIsExpected;
+			const invalidVariant4: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-40ab-1234567890ab">> = errorIsExpected;
+			const invalidVariant5: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-50ab-1234567890ab">> = errorIsExpected;
+			const invalidVariant6: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-60ab-1234567890ab">> = errorIsExpected;
+			const invalidVariant7: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-70ab-1234567890ab">> = errorIsExpected;
+			const invalidVariantC: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-c0ab-1234567890ab">> = errorIsExpected;
+			const invalidVariantD: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-d0ab-1234567890ab">> = errorIsExpected;
+			const invalidVariantE: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-e0ab-1234567890ab">> = errorIsExpected;
+			const invalidVariantF: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-f0ab-1234567890ab">> = errorIsExpected;
+			const invalidVariantC2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-C0ab-1234567890ab">> = errorIsExpected;
+			const invalidVariantD2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-D0ab-1234567890ab">> = errorIsExpected;
+			const invalidVariantE2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-E0ab-1234567890ab">> = errorIsExpected;
+			const invalidVariantF2: ExpectAssertionError<UUIDv4.Assert<"12345678-abcd-4123-F0ab-1234567890ab">> = errorIsExpected;
 		}
 		// #endregion
 
 		// #region positive type test
 		{
 			{
-				const v = "12345678-abcd-4123-5678-1234567890ab";
+				const v = "12345678-abcd-4123-890a-1234567890ab";
+				const a: UUIDv4.Assert<typeof v> = v;
+				const i = new IdentifiableError<typeof a>(a);
+				console.assert(i.id == a);
+			}
+			{
+				const v = "12345678-abcd-4123-990a-1234567890ab";
+				const a: UUIDv4.Assert<typeof v> = v;
+				const i = new IdentifiableError<typeof a>(a);
+				console.assert(i.id == a);
+			}
+			{
+				const v = "12345678-abcd-4123-a90a-1234567890ab";
+				const a: UUIDv4.Assert<typeof v> = v;
+				const i = new IdentifiableError<typeof a>(a);
+				console.assert(i.id == a);
+			}
+			{
+				const v = "12345678-abcd-4123-A90a-1234567890ab";
+				const a: UUIDv4.Assert<typeof v> = v;
+				const i = new IdentifiableError<typeof a>(a);
+				console.assert(i.id == a);
+			}
+			{
+				const v = "12345678-abcd-4123-b90a-1234567890ab";
+				const a: UUIDv4.Assert<typeof v> = v;
+				const i = new IdentifiableError<typeof a>(a);
+				console.assert(i.id == a);
+			}
+			{
+				const v = "12345678-abcd-4123-B90a-1234567890ab";
 				const a: UUIDv4.Assert<typeof v> = v;
 				const i = new IdentifiableError<typeof a>(a);
 				console.assert(i.id == a);
