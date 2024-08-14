82942d25d135098ba1b41455cc01d02475463fe4
Kisaragi <48310258+KisaragiEffective@users.noreply.github.com>
2024-08-15 01:38:01 +0900

    chore: allow only lowercase

--- packages/backend/src/misc/uuid-validator.ts
+++ packages/backend/src/misc/uuid-validator.ts
@@ -27,7 +27,7 @@ declare module FoldableString {
 declare module Hex {
 	const notHex: unique symbol;
 
-	type HexChar = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "a" | "b" | "c" | "d" | "e" | "f" | "A" | "B" | "C" | "D" | "E" | "F";
+	type HexChar = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "a" | "b" | "c" | "d" | "e" | "f";
 
 	type Assert0<S extends string, Counter extends Counter.Sequence> = FoldableString.Chars<S>[Counter.Current<Counter>] extends HexChar
 		? Counter.Current<Counter.Increase<Counter>> extends FoldableString.LengthS<S>
